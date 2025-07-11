b953f0e5216a9359eead8a6749c6f8438af9868a
nh1nxncy <147337010+nh1nxncy@users.noreply.github.com>
2025-07-11 01:32:40 -0400

    modified widget test

--- closetinventory/test/widget_test.dart
+++ closetinventory/test/widget_test.dart
@@ -5,26 +5,34 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:closetinventory/views/modules/dashcard_module.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:closetinventory/main.dart';
+import 'package:provider/provider.dart';
+import 'package:closetinventory/controllers/utilities/theme_provider.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Closet Dashboard loads and shows expected UI', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      ChangeNotifierProvider<ThemeProvider>(
+        create: (context) => ThemeProvider(),
+        child: MaterialApp(
+          home: DashCard(
+            title: 'My Closet Dashboard',
+            icon: Icons.checkroom,
+            number: 0,
+            color: Colors.blue,
+            link: '/dashboard',
+          ),
+        ),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
+    await tester.pumpAndSettle();
+
+    expect(find.text('My Closet Dashboard'), findsOneWidget);
     expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
